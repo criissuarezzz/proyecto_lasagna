9374a9aa8b3d63e3f5ee6c2fbb1cdf925f5d807c
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2022-09-29 17:34:40 +0200

    commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ EXPECTED_BAKE_TIME= 40 #minutos que tarda en hornearse la lasaña
 PREPARATION_TIME= 2 #minutos que tarda cada capa en hacerse
 def bake_time_remaining(elapsed_bake_time):      #el tiempo que le queda para terminar de hornear
     return (EXPECTED_BAKE_TIME - elapsed_bake_time) #resta el tiempo que tarda en hornear y el que lleva
-def preparation_time_in_minutes (number_of_layers): #
+def preparation_time_in_minutes (number_of_layers): #cuánto se tarda en hacer la lasaña en funación de las capas que tenga
     return number_of_layers * PREPARATION_TIME
-def elapsed_time_in_minutes (number_of_layers, elapsed_bake_time):
+def elapsed_time_in_minutes (number_of_layers, elapsed_bake_time): #el tiempo que ha tardado en función de las capas y el horneado
     return preparation_time_in_minutes(number_of_layers)+elapsed_bake_time
